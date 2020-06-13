9b389a7f2caa33e783d3883049de4e33a10978f4
piemekanika@hotmail.com <piemekanika@hotmail.com>
2020-06-13 11:11:07 +0700

    fixed a typo

--- src/app.gateway.spec.ts
+++ src/app.gateway.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ApGateway } from './app.gateway';
+import { AppGateway } from './app.gateway';
 
 describe('ApGateway', () => {
-  let gateway: ApGateway;
+  let gateway: AppGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ApGateway],
+      providers: [AppGateway],
     }).compile();
 
-    gateway = module.get<ApGateway>(ApGateway);
+    gateway = module.get<AppGateway>(AppGateway);
   });
 
   it('should be defined', () => {
